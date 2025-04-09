78c1e4d2517aadebee7d16a0f22e54d2ac721a36
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2025-04-08 22:27:57 -0300

    Primer commit, inicializacion de base de datos y pruebas

---
+++ MyConsoleApp/Models/blogging.cs
@@ -0,0 +1,78 @@
+/*
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace MyConsoleApp.Models
+{
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    public string DbPath { get; }
+
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; } = new();
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
+}*/
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MyConsoleApp.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public List<Post> Posts { get; set; } = new();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; } = null!;
+    }
+}
---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using MyConsoleApp.Models;
+
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creamos el contenedor de dependencias
+            var services = new ServiceCollection();
+
+            // Llamamos a Startup para configurar servicios
+            var startup = new Startup();
+            startup.ConfigureServices(services);
+
+            // Construimos el proveedor
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Obtenemos el contexto de base de datos desde DI
+            using var context = serviceProvider.GetRequiredService<BloggingContext>();
+
+            // Acá podés hacer algo con el contexto, por ejemplo:
+            context.Database.EnsureCreated();
+
+            context.Blogs.Add(new Blog { Url = "https://ejemplo.com" });
+            context.SaveChanges();
+
+            foreach (var blog in context.Blogs)
+            {
+                Console.WriteLine($"Blog: {blog.BlogId} - {blog.Url}");
+            }
+        }
+    }
+}
